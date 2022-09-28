c4839d6c540c708a1a68bf777f606960cf0a3ffb
ioritz <ioritzurkiri2002@gmail.com>
2022-09-28 11:55:35 +0200

    package info created in all packages

--- src/reto0din/model/package-info.java
+++ src/reto0din/model/package-info.java
@@ -1,6 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This package contains the clases, iterfaces and config files for the model
  */
 package reto0din.model;
